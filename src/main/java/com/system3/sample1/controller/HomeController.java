1aba9e40e43b9934e4ac92ab934fb410d3b5cb87
NOBUYUKI OTSU <ootsunobuyuki@gmail.com>
2020-04-03 15:29:20 +0900

    first

---
+++ src/main/java/com/system3/sample1/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.system3.sample1.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+	@GetMapping("/index")
+	public String index() {
+		return "index";
+	}
+}
